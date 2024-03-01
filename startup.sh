dbf324eea1ebbcd67c2fa27b7078d92357ecce32
Harsha Meka <157233599+Harsha-M3K4@users.noreply.github.com>
2024-03-01 01:37:31 -0500

    Update startup.sh with command for opening new terminal tab

--- startup.sh
+++ startup.sh
@@ -13,7 +13,8 @@ gnome-terminal --tab -- bash -c "roslaunch audubon_unity audubon_unity.launch; e
 # Sleep for a bit to ensure that the previous command has time to start up
 sleep 4
 
-# Open a new tab, go to the directory and run the file
-gnome-terminal --tab --working-directory="/home/<user_name>/Desktop/F1Tenth_Simulator_Linux" -- bash -c "cd /home/<user_name>/Desktop/F1Tenth_Simulator_Linux && ./F1Tenth_Simulator_Linux.x86_64; exec bash" #Launches F1Tenth_Simulator_Linux.x86_64 in a new terminal tab
+# Open a new tab, goes to the directory and runs the simulator file. Replace <user_name> with your machine username
+gnome-terminal --tab --working-directory="/home/<user_name>/Desktop/F1Tenth_Simulator_Linux" -- bash -c "cd /home/<user_name>/Desktop/F1Tenth_Simulator_Linux && ./F1Tenth_Simulator_Linux.x86_64; exec bash" 
 
-cd
+#Opens a new terminal tab
+gnome-terminal --tab
